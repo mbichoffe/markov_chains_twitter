a4ec4eabe7d1c7d2a94ef1e198f4ac47e6451be2
mbichoffe <bichoffe.marina@gmail.com>
2017-12-04 13:21:18 -0800

    added oauth for sign-in

--- markov.py
+++ markov.py
@@ -1,101 +1,154 @@
 #!/usr/bin/env python
-"""Generate Markov text from text files."""
-import sys
+# -*- coding: utf-8 -*-
+import os, sys
+import re
+import twitter
 from random import choice
 from textblob import TextBlob #NLP 
 
-def open_and_read_file(filename):
-    """Take file path as string; return text as string.
 
-    Takes a string that is a file path, opens the file, and turns
-    the file's contents as one string of text.
-    """
-    f = open(filename, 'rU')
-    text = f.read()
-    f.close()
+class MarkovGenerator(object):
+    """A Markov chain text generator"""
 
-    return text
+    def open_and_read_file(self, filenames):
+        """Take file(s) as tuples; return text as string.
 
-
-def make_chains(text_string):
-    """Take input text as string; return dictionary of Markov chains.
-
-    A chain will be a key that consists of a tuple of (word1, word2)
-    and the value would be a list of the word(s) that follow those two
-    words in the input text.
-
-    For example:
-
-        >>> chains = make_chains("hi there mary hi there juanita")
-
-    Each bigram (except the last) will be a key in chains:
-
-        >>> sorted(chains.keys())
-        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
-
-    Each item in chains is a list of all possible following words:
-
-        >>> chains[('hi', 'there')]
-        ['mary', 'juanita']
-
-        >>> chains[('there','juanita')]
-        [None]
-    """
-    markov_chains = {}
-    words = text_string.split()
-    for i in range(len(words)-2):
-        bigram = (words[i], words[i+1])
-        next_word = words[i+2]
-        markov_chains.setdefault(bigram, [])
-        markov_chains[bigram].append(next_word)
-
-    return markov_chains
+        Takes one or more strings that are a file path, opens the file,
+        and turns the files' contents as one string of text.
+        """
+        body = []
+        print filenames
+        for filename in filenames:
+            f = open(filename, 'rU')
+            text = f.read()
+            body.append(text)
+            f.close()
+        # create single string
+        body = ' '.join(body)
+        self.make_chains(body)
 
 
-def make_text(chains):
-    """Return text from chains."""
+    def make_chains(self, text_string, n=2):
+        """Take input text as string; return dictionary of Markov chains.
 
-    #container for generated text
-    words = []
+        A chain will be a key that consists of a tuple of (word1, word2)
+        and the value would be a list of the word(s) that follow those two
+        words in the input text.
+        n is an integer indicating the number of items used to generate the n-grams.
+        It is usually 2 or 3. If no number is specified, a bigram will be generated.
 
-    link = choice(chains.keys())
-    words += link[0], link[1]
-    next_word = choice(chains[link])
+        For example:
 
-    while next_word is not None:
-        link = (link[1], next_word)
-        words.append(next_word)
-        next_word = chains.get(choice(chains[link]))
+            >>> chains = make_chains("hi there mary hi there juanita")
 
-    print words
-    return " ".join(words)
+        Each bigram (except the last) will be a key in chains:
+
+            >>> sorted(chains.keys())
+            [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+
+        Each item in chains is a list of all possible following words:
+
+            >>> chains[('hi', 'there')]
+            ['mary', 'juanita']
+
+            >>> chains[('there','juanita')]
+            [None]
+        """
+        self.chains = {}
+        words = text_string.split()
+        for i in range(len(words)-n):
+            ngram = (words[i], words[i+1])
+            next_word = words[i+n]
+            self.chains.setdefault(ngram, [])
+            self.chains[ngram].append(next_word)
+
+
+    def stop_text(self, words, new_word):
+        """Should we stop making text?
+
+        In this base generator, we should continue until we reach the
+        end of our chain; in subclasses, we might stop on other conditions.
+        """
+
+        # This is just one way to think about this; there are many others.
+        #punctuation for now
+        ends_in_punct = new_word[-1] in ([".", "?", "!"])
+
+        passed_char_limit = len(" ".join(words)) + len(new_word) > 140
+
+        return passed_char_limit or ends_in_punct
+
+
+    def make_text(self):
+        """Take dictionary of markov chains; returns random text."""
+
+        #character limit
+        #punctuation
+        #bad words
+        #container for generated text
+        words = []
+
+        link = choice(self.chains.keys())#NPL
+        words += link[0], link[1]
+
+        while link in self.chains:
+            # Keep looping until we have a key that isn't in the chains
+            # (which would mean it was the end of our original text)
+            #
+            # Note that for long texts (like a full book), this might mean
+            # it would run for a very long time.
+            next_word = choice(self.chains[link])
+            # Should we stop here?
+            if self.stop_text(words, next_word):
+                break
+
+            words.append(next_word)
+            link = (link[1], next_word)#create new ngram
+            next_word = choice(self.chains[link])
+
+        return " ".join(words)
+
+    def make_and_post_tweets(random_text, markov_chains):
+        """Create a tweet and send it to the Internet."""
+
+        # Use Python os.environ to get at environmental variables
+        # Note: run `source secrets.sh` before running this file
+        # to make sure these environmental variables are set.
+        api = twitter.Api(
+            consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+            consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+            access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+            access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+
+        while True:
+            status = api.PostUpdate(make_text(markov_chains))
+            print status.text
+            print  # blank line
+            response = raw_input("Enter to tweet again [q to quit] > ")
+            if response.lower() == 'q':
+                break
 
 
 def main():
 
     args = sys.argv[1:]
     if not args:
-        print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
+        print "usage: textfile.txt [textfile2.txt...]"
         sys.exit(1)
 
-    # The args array is left just containing the dirs.
-   
-    if len(args) == 0:
-        print "error: must specify one or more dirs"
-        sys.exit(1)
+    print "\n\n\nRegular Generator"
 
-  # Call functions
+    generator = MarkovGenerator()
+    generator.open_and_read_file(args)
 
-    # Open the file and turn it into one long string
-    input_text = open_and_read_file(input_path)
+    for i in range(5):
+        print generator.make_text()
+        print
 
-    # Get a Markov chain
-    markov_chains = make_chains(input_text)
+    #post tweets using Twitter API
+    #make_and_post_tweets(random_text, markov_chains)
 
-    # Produce random text
-    random_text = make_text(markov_chains)
-
-    print random_text
+    #print random_text
 
 if __name__ == "__main__":
     main()
--- server.py
+++ server.py
@@ -1,22 +1,118 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, flash, redirect, session, \
     jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 from markov import *
+import twitter
 import requests
 from jinja2 import StrictUndefined
 import json
+import urlparse
+import oauth2 as oauth
 # import calendar
 app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "AIRSPEEDVELOCITYOFANUNLADENSWALLOW"
+TWITTER_CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
+TWITTER_CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
+TWITTER_ACCESS_TOKEN_KEY = os.environ.get('TWITTER_ACCESS_TOKEN_KEY')
+TWITTER_ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
 
+###ENDPOINTS###
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZE_URL = 'https://api.twitter.com/oauth/authorize'
+RETURN_URL = 'http://localhost:5000/oauth'
 
 @app.route('/')
 def index():
-    
+    args = ["gettysburg.txt"]
+    generator = MarkovGenerator()
+    generator.open_and_read_file(args)
+    tweets = []
+    for i in range(5):
+        tweets.append(generator.make_text())
+    print tweets
+    return render_template("index.html", tweets=tweets)
     """Main page."""
+
+@app.route('/register')
+def get_oauth_token():
+    """
+    Send oauth request to twitter API and get token or display errors.
+    If successfull, will redirect user to Twitter's Approve/Deny page.
+    """
+    consumer = oauth.Consumer(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+    client = oauth.Client(consumer)
+    # Get oauth request token:
+    resp, content = client.request(REQUEST_TOKEN_URL, "GET")
+    #Any value other than 200 on status indicates a failure:
+    if resp['status'] != '200':
+            flash("Invalid response {}".format(resp['status']))
+    # parse response params
+    request_token = dict(urlparse.parse_qsl(content))
+    # check if callback is confirmed, else flash errors. 
+    if request_token['oauth_callback_confirmed']:
+        oauth_token = request_token['oauth_token']
+        session['oauth_token'] = oauth_token #save in session
+        oauth_token_secret = request_token['oauth_token_secret']
+        session['oauth_token_secret'] = oauth_token_secret
+        # now we will redirect the user to Twitter's authorization page
+        return redirect(AUTHORIZE_URL
+                        + "?oauth_token=" 
+                        + oauth_token)
+    # if callback is not confirmed, flash errors and return to main page
+    errors = request_token.get('errors')
+    flash('error code: ', errors['code'], 'message :', errors['message'])
+    return redirect('/')
+
+
+@app.route("/oauth")
+def oauth_process():
+    """Redirect user to Twitter's Approve/Deny page."""
+
+    # Upon a successful authentication we should have received a request
+    # containing the oauth_token and oauth_verifier parameters.
+    # the oauth token should be the same as received in /register.
+
+    oauth_verifier = request.args.get('oauth_verifier')
+    # TODO: check for oauth token
+    if oauth_verifier:
+        access_token = get_access_token(oauth_verifier)
+
+        if access_token:
+            return redirect('/home')
+
+    # If there is no access code, flash an error message
+    else:
+        flash('OAuth failed')
+
+    return redirect('/')
+
+
+
+######### Helper Functions #########
+
+def get_access_token(oauth_verifier):
+    """Use access code to request user's access token"""
+
+    consumer = oauth.Consumer(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
+    token = oauth.Token(session['oauth_token'],
+                        session['oauth_token_secret'])
+    token.set_verifier(oauth_verifier)
+    client = oauth.Client(consumer, token)
+    # Make a post request with the oauth verifier
+    resp, content = client.request(ACCESS_TOKEN_URL, "POST")
+    # Any value other than 200 on response indicates failure:
+    if resp['status'] != '200':
+            access_token = None
+            flash("Invalid response {}".format(resp['status']))
+    # if sucessfull, save oauth token in session
+    access_token = dict(urlparse.parse_qsl(content))
+
+    return access_token
 
 
 app.jinja_env.undefined = StrictUndefined
